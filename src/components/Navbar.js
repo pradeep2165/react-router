13399a1ef55c3e22e61794a30eb483a335eafeae
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-13 14:50:30 +0530

    active links with component pass by style attribute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const navLinkStyles = ({ isActive }) => {
+  return {
+    fontWeight: isActive ? "bold" : "normal",
+    textDecoration: isActive ? "none" : "underline",
+  };
+};
+
 function Navbar() {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <NavLink style={navLinkStyles} to="/">
+        Home
+      </NavLink>
+      <NavLink style={navLinkStyles} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 }
